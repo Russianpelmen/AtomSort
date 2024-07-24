ccdf575dcc109c1a137c4bcd29fa69fe61defb0e
User <User@WIN-VEJCSFIRQOG>
2024-07-24 19:24:09 +0400

    Добавьте файлы проекта.

---
+++ AtomSort/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#define ArrLenght 10
+
+// Algorithm version
+auto AtomSortLambda = [](const int* arr, const int& size) -> int* { int* NewArr = new int[size]; for (int i = 0; i < size; i++) NewArr[i] = NULL; return NewArr; };
+int* AtomSortFunction(const int* arr, const int& size) { int* NewArr = new int[size]; for (int i = 0; i < size; i++) NewArr[i] = NULL; return NewArr; };
+
+void PrintArray(const int* arr, const int& size) {
+	for (int i = 0; i < size; ++i) std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+int main() {
+
+	int Array[ArrLenght] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+	PrintArray(Array, ArrLenght);
+	PrintArray(AtomSortFunction(Array, ArrLenght), ArrLenght);
+
+	return 0;
+}
